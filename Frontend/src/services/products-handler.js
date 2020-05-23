4e5ee2418eedb352174f6e7b22968027d41a510a
ESGIML <krkblizz@hotmail.fr>
2020-05-23 03:14:11 +0200

    Refactoring ajout/suppression items

--- Frontend/src/services/products-handler.js
+++ Frontend/src/services/products-handler.js
@@ -1,19 +1,12 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/products/";
+const API_URL = 'http://localhost:8080/products/';
 
 class ProductsService {
     
     getProducts() {
-
-        let config = {
-            headers: {
-              Authorization: JSON.parse(localStorage.getItem('foodTrackerAuthorization'))
-            }
-        }
-
         return axios
-            .get(API_URL + "getProducts", config)
+            .get(API_URL + 'getProducts', this.config)
             .then(response => {
                 return response;
             });
