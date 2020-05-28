8fe6321ee27cd277071707b113f4cba0f61872a5
Ilès Bnk <ilesbnk@hotmail.fr>
2020-05-28 17:03:01 +0200

    Add transition between camera and no camera available + image preview

--- Frontend/src/components/paniers.js
+++ Frontend/src/components/paniers.js
@@ -4,6 +4,9 @@ import CustomCarousel from './customCarousel.js';
 import AuthService from '../services/auth-service';
 import axios from "axios";
 import ProfilePopover from './profile_popover.js';
+import Camera, { FACING_MODES, IMAGE_TYPES } from 'react-html5-camera-photo';
+import 'react-html5-camera-photo/build/css/index.css';
+import ImagePreview from './ImagePreview';
 
 import {
   EuiPage,
@@ -36,10 +39,19 @@ export default function Home() {
   const [file, setFile] = useState(buildFileSelector());
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [quantity, setQuantity] = useState('1');
+  const [dataUri, setDataUri] = useState('');
+  const [photoMode, setphotoMode] = useState(false);
+  const [hasCamera, sethasCamera] = useState(false);
   const closeModal = () => {
     setIsModalVisible(false);
     let elementsEuiOverlay = document.getElementsByClassName('euiOverlayMask');
     elementsEuiOverlay[0].remove();
+  }
+
+  function handleTakePhoto(dataUri) {
+    setDataUri(dataUri);
+    setphotoMode(false);
+    showModal();
   }
 
   const showModal = () => setIsModalVisible(true);
@@ -57,6 +69,18 @@ export default function Home() {
       .then((response) => {
         setItems(response.data);
       })
+
+    navigator.getMedia = (navigator.getUserMedia ||
+      navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia ||
+      navigator.msGetUserMedia);
+
+    navigator.getMedia({ video: true }, function () {
+      sethasCamera(true);
+    }, function () {
+      sethasCamera(false);
+    });
+
     // eslint-disable-next-line
   }, [])
 
@@ -127,8 +151,13 @@ export default function Home() {
   ];
 
   const handleFileSelect = (e) => {
-    e.preventDefault();
-    file.click();
+    if (hasCamera) {
+      setphotoMode(true);
+    }
+    else {
+      e.preventDefault();
+      file.click();
+    }
   }
 
   const getProducts = () => {
@@ -143,9 +172,30 @@ export default function Home() {
     addNewItem();
   }
 
+  function DataURIToBlob(dataURI) {
+    const splitDataURI = dataURI.split(',')
+    const byteString = splitDataURI[0].indexOf('base64') >= 0 ? atob(splitDataURI[1]) : decodeURI(splitDataURI[1])
+    const mimeString = splitDataURI[0].split(':')[1].split(';')[0]
+
+    const ia = new Uint8Array(byteString.length)
+    for (let i = 0; i < byteString.length; i++)
+      ia[i] = byteString.charCodeAt(i)
+
+    return new Blob([ia], { type: mimeString })
+  }
+
+
   function addNewItem() {
     const data = new FormData();
-    data.append('image', file.files[0]);
+
+    if(hasCamera) {
+      const file = DataURIToBlob(dataUri);
+      data.append('image', file, 'image.jpg');
+    }
+    else {
+      data.append('image', file.files[0]);
+    }
+
     data.append("quantity", quantity);
     axios.post(API_URL_PRODUCTS + 'addProduct', data, config).then(() => {
       getProducts().then((response) => {
@@ -191,6 +241,8 @@ export default function Home() {
           <EuiModalHeaderTitle>Définir la quantité</EuiModalHeaderTitle>
         </EuiModalHeader>
         <EuiModalBody>
+          <ImagePreview dataUri={dataUri} />
+          <EuiSpacer />
           <EuiFieldNumber
             placeholder="1"
             value={quantity}
@@ -208,65 +260,80 @@ export default function Home() {
   )
 
   if (AuthService.getCurrentUser()) {
-    return (
-      <EuiPage>
-        {isModalVisible && modal}
-        <EuiPageBody>
-          <EuiPageContent>
-            <EuiPageContentHeader>
-              <EuiPageContentHeaderSection style={{ "width": "100%" }}>
-                <EuiFlexGroup justifyContent="spaceBetween" style={{ "width": "100%" }}>
-                  <EuiFlexItem grow={false} tyle={{ minWidth: 200 }}>
-                    <EuiTitle size='l'>
-                      <h1>Food Tracker</h1>
-                    </EuiTitle>
-                  </EuiFlexItem>
-                  <EuiFlexItem grow={false}>
-                    <ProfilePopover />
-                  </EuiFlexItem>
-                </EuiFlexGroup>
-              </EuiPageContentHeaderSection>
-            </EuiPageContentHeader>
-            <EuiPageContentBody>
-              <EuiFlexGroup>
-                <EuiFlexItem>
-                  <EuiTitle size='m'>
-                    <h3>Aliments</h3>
-                  </EuiTitle>
-                  <EuiBasicTable
-                    items={items}
-                    columns={columns}
-                    hasActions={true}
-                  />
-                  <EuiSpacer />
-                  <EuiFlexGroup justifyContent="spaceAround">
+
+    if (photoMode) {
+      return (
+        <Camera
+          onTakePhoto={(dataUri) => { handleTakePhoto(dataUri); }}
+          isFullscreen={false}
+          idealFacingMode={FACING_MODES.ENVIRONMENT}
+          imageType={IMAGE_TYPES.JPG}
+          sizeFactor={1}
+          isImageMirror={false}
+        />
+      );
+    }
+    else {
+      return (
+        <EuiPage>
+          {isModalVisible && modal}
+          <EuiPageBody>
+            <EuiPageContent>
+              <EuiPageContentHeader>
+                <EuiPageContentHeaderSection style={{ "width": "100%" }}>
+                  <EuiFlexGroup justifyContent="spaceBetween" style={{ "width": "100%" }}>
+                    <EuiFlexItem grow={false} tyle={{ minWidth: 200 }}>
+                      <EuiTitle size='l'>
+                        <h1>Food Tracker</h1>
+                      </EuiTitle>
+                    </EuiFlexItem>
                     <EuiFlexItem grow={false}>
-                      <EuiButton iconType="plusInCircleFilled" onClick={handleFileSelect}>
-                        Ajouter un article
-                      </EuiButton>
+                      <ProfilePopover />
                     </EuiFlexItem>
                   </EuiFlexGroup>
-                </EuiFlexItem>
-                <EuiFlexItem>
-                  <EuiTitle size='m' style={{ margin: 'auto' }}>
-                    <h2>Ma liste de course</h2>
-                  </EuiTitle>
-                  <EuiText textAlign="center">
-                    <h3>Paniers automatiques</h3>
-                  </EuiText>
-                  <CustomCarousel />
-                  <EuiSpacer />
-                </EuiFlexItem>
-              </EuiFlexGroup>
-              <EuiSpacer />
-              <EuiLink href="/home" >
-                Home
+                </EuiPageContentHeaderSection>
+              </EuiPageContentHeader>
+              <EuiPageContentBody>
+                <EuiFlexGroup>
+                  <EuiFlexItem>
+                    <EuiTitle size='m'>
+                      <h3>Aliments</h3>
+                    </EuiTitle>
+                    <EuiBasicTable
+                      items={items}
+                      columns={columns}
+                      hasActions={true}
+                    />
+                    <EuiSpacer />
+                    <EuiFlexGroup justifyContent="spaceAround">
+                      <EuiFlexItem grow={false}>
+                        <EuiButton iconType="plusInCircleFilled" onClick={handleFileSelect}>
+                          Ajouter un article
+                      </EuiButton>
+                      </EuiFlexItem>
+                    </EuiFlexGroup>
+                  </EuiFlexItem>
+                  <EuiFlexItem>
+                    <EuiTitle size='m' style={{ margin: 'auto' }}>
+                      <h2>Ma liste de course</h2>
+                    </EuiTitle>
+                    <EuiText textAlign="center">
+                      <h3>Paniers automatiques</h3>
+                    </EuiText>
+                    <CustomCarousel />
+                    <EuiSpacer />
+                  </EuiFlexItem>
+                </EuiFlexGroup>
+                <EuiSpacer />
+                <EuiLink href="/home" >
+                  Home
               </EuiLink>{' '}
-            </EuiPageContentBody>
-          </EuiPageContent>
-        </EuiPageBody>
-      </EuiPage>
-    );
+              </EuiPageContentBody>
+            </EuiPageContent>
+          </EuiPageBody>
+        </EuiPage>
+      );
+    }
   }
   else {
     window.location.href = '/login';
